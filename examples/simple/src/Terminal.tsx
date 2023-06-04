0919839b89ab082d87e3844e14c48859623f5690
Imran Dhahbi <playforest0x@gmail.com>
2023-06-04 15:35:45 +1000

    dynamic smooth updates of leading placeholder and data array (sliced by firstRenderedElementIndex)

--- examples/simple/src/Terminal.tsx
+++ examples/simple/src/Terminal.tsx
@@ -1,12 +1,67 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, RefObject, useCallback, useState } from "react";
 import { DataStream } from './helpers/DataStream'
 import { useSelector, useDispatch } from 'react-redux';
 import { pushData } from './features/datastream/datastreamSlice';
 import { RootState } from "./rootState";
+import { VirtualConsole } from "./virtualconsole/VirtualConsole";
 
 export const Terminal = () => {
     const dispatch = useDispatch();
     const data = useSelector((state: RootState) => state.datastream.data);
+    const [scrollPosition, setScrollPosition] = useState(0);
+
+    const contentRef: RefObject<HTMLDivElement> = useRef(null);
+    // const lineRef: RefObject<HTMLDivElement> = useRef(null);
+    const leadingDivPlaceholderRef: RefObject<HTMLDivElement> = useRef(null);
+
+    let lineRef: HTMLDivElement | null = null;
+    let virtualconsole = useRef<VirtualConsole | null>(null);
+
+    const setLineRef = useCallback((node: HTMLDivElement | null) => {
+        if (node !== null) {
+            lineRef = node;
+            if (virtualconsole.current === null) {
+                virtualconsole.current = new VirtualConsole({
+                    _totalLines: data,
+                    _currentScrollPosition: contentRef.current?.scrollTop ?? 0,
+                    _viewableAreaHeight: contentRef.current?.clientHeight ?? 0,
+                    _lineHeight: lineRef.clientHeight,
+                    _buffer: 5
+                });
+            }
+        }
+    }, [data, contentRef]);
+
+    let leadingPlaceholderHeight = 0;
+
+    useEffect(() => {
+        const scrollHandler = (event: any) => {
+            // Update the virtualconsole properties on each scroll
+            if (virtualconsole.current) {
+                let scrollPos = contentRef.current?.scrollTop;
+                setScrollPosition(scrollPos ?? 0);
+                // virtualconsole.current.totalLines = data;
+
+                console.log('scrollTop pos: ', virtualconsole.current.currentScrollPosition)
+                virtualconsole.current.currentScrollPosition = scrollPos ?? 0;
+                // virtualconsole.current.currentScrollPosition = scrollPos ?? 0;
+
+                virtualconsole.current.updateLeadingPlaceholderHeight();
+
+                const leadingPlaceholderHeight = virtualconsole.current.leadingPlaceholderHeight;
+                console.log('leadingPlaceholderHeight: ', leadingPlaceholderHeight)
+                console.log('firstVIsibleElementIndex: ', virtualconsole.current.firstRenderedElementIndex())
+                leadingDivPlaceholderRef.current?.style.setProperty('height', `${leadingPlaceholderHeight}px`);
+            }
+        }
+
+        const el = contentRef.current;
+        if (el) {
+            el.addEventListener("scroll", scrollHandler, { passive: true });
+            return () => el.removeEventListener("scroll", scrollHandler);
+        }
+    }, []);
+
 
     useEffect(() => {
         console.log('Creating new instance of DataStream and connecting');
@@ -22,12 +77,31 @@ export const Terminal = () => {
             }
         })
         stream.connect();
+
     }, [])
 
     return (
-        <div className="content">
-            <div className="prompt">
-                {data.map((item, index) => (<div className="line" key={index}> <span style={{ color: '#27c93f' }}>/dev/ttyUSB0$</span> <span>{item}</span></div>))}
+        <div
+            className="content" ref={contentRef}>
+            <div
+                className="prompt">
+                <div
+                    className="leading-placeholder"
+                    ref={leadingDivPlaceholderRef}
+                >
+                </div>
+                {data.slice(virtualconsole.current?.firstRenderedElementIndex() ?? 0)
+                    .map((item, index) => (
+                        <div className="line" key={`${item}-${index}`} ref={setLineRef}>
+                            <span style={{ color: '#27c93f', lineHeight: '14px' }}>/dev/ttyUSB0$</span>
+                            <span>{item}</span>
+                        </div>
+                    ))
+                }
+                <div
+                    className="trailing-placeholder"
+                    style={{ height: '0px' }}>
+                </div>
             </div>
         </div>
     )
--- examples/simple/src/virtualconsole/VirtualConsole.ts
+++ examples/simple/src/virtualconsole/VirtualConsole.ts
@@ -1,47 +1,144 @@
 interface VirtualConsoleProps {
-    totalLines: string[],
-    viewableAreaHeight: number,
-    lineHeight: number,
-    visibleLineCount: number,
-    currentScrollPosition: number,
-    buffer: number,
-
+    _totalLines: string[],
+    _viewableAreaHeight: number,
+    _lineHeight: number,
+    _currentScrollPosition: number,
+    _buffer: number,
+    // _leadingPlaceholderHeight: number,
+    // _trailingDivHeight: number
 }
 
 export class VirtualConsole {
-    private totalLines: string[];
-    private viewablAreaHeight: number;
-    private lineHeight: number;
-    private visibleLineCount: number;
-    private currentScrollPosition: number;
-    private buffer: number;
+    private _totalLines: string[];
+    private _viewablAreaHeight: number;
+    private _lineHeight: number;
+    private _visibleLineCount: number;
+    private _currentScrollPosition: number;
+    private _buffer: number;
+    private _leadingPlaceholderHeight: number = 0;
+    // private _trailingDivHeight: number;
 
     constructor(props: VirtualConsoleProps) {
-        this.totalLines = props.totalLines;
-        this.currentScrollPosition = props.currentScrollPosition;
-        this.viewablAreaHeight = props.viewableAreaHeight;
-        this.lineHeight = props.lineHeight;
-        this.buffer = props.buffer;
-        this.visibleLineCount = this.calculateVisibleLineCount(props.viewableAreaHeight, props.lineHeight)
+        this._totalLines = props._totalLines;
+        this._viewablAreaHeight = props._viewableAreaHeight;
+        this._lineHeight = props._lineHeight;
+        this._currentScrollPosition = props._currentScrollPosition;
+        this._buffer = props._buffer;
+        this._visibleLineCount = this.visibleLines(this._viewablAreaHeight, this._lineHeight);
+
+        this.updateLeadingPlaceholderHeight();
     }
 
-    public calculateVisibleLineCount(viewablAreaHeight: number, lineHeight: number): number {
+    // Getters & Setters
+    public get totalLines(): string[] {
+        return this._totalLines;
+    }
+
+    public set totalLines(value: string[]) {
+        this._totalLines = value;
+    }
+
+    public get viewablAreaHeight(): number {
+        return this._viewablAreaHeight;
+    }
+
+    public set viewablAreaHeight(value: number) {
+        this._viewablAreaHeight = value;
+        this._visibleLineCount = this.visibleLines(this._viewablAreaHeight, this._lineHeight); // Recalculate visibleLineCount
+    }
+
+    public get lineHeight(): number {
+        return this._lineHeight;
+    }
+
+    public set lineHeight(value: number) {
+        this._lineHeight = value;
+        this._visibleLineCount = this.visibleLines(this._viewablAreaHeight, this._lineHeight); // Recalculate visibleLineCount
+    }
+
+    public get currentScrollPosition(): number {
+        return this._currentScrollPosition;
+    }
+
+    public set currentScrollPosition(value: number) {
+        this._currentScrollPosition = value;
+    }
+
+    public get buffer(): number {
+        return this._buffer;
+    }
+
+    public set buffer(value: number) {
+        this._buffer = value;
+    }
+
+    // VisibleLineCount only has a getter since it's calculated based on other properties
+    public get visibleLineCount(): number {
+        return this._visibleLineCount;
+    }
+
+    public visibleLines(viewablAreaHeight: number, lineHeight: number): number {
         return Math.floor(viewablAreaHeight / lineHeight);
     }
 
-    private calculateFirstVisibleElementIndex(): number {
-        return Math.floor(this.currentScrollPosition / this.lineHeight);
+    public firstVisibleElementIndex(): number {
+        return Math.floor(this._currentScrollPosition / this._lineHeight) ?? 0;
     }
 
-    private calculateLastVisibleElementIndex(): number {
-        return this.calculateFirstVisibleElementIndex() + (this.viewablAreaHeight / this.lineHeight);
+    public firstRenderedElementIndex(): number {
+        if (this.firstVisibleElementIndex() < this._buffer) {
+            return 0;
+        }
+        return this.firstVisibleElementIndex() - this._buffer
     }
 
-    private calculateLeadingPlaceholderHeight(): number {
-        return (this.totalLines.length - this.calculateFirstVisibleElementIndex() - this.buffer) * this.lineHeight;
+    private lastVisibleElementIndex(): number {
+        return this.firstVisibleElementIndex() + (this._viewablAreaHeight / this._lineHeight);
     }
 
-    private calculateTrailingPlaceholderHeight(): number {
-        return (this.totalLines.length - this.calculateLastVisibleElementIndex() + this.buffer) * this.lineHeight;
+    private lastRenderedElementIndex(): number {
+        if (this.lastVisibleElementIndex() < (this.totalLines.length - this._buffer)) {
+            return this.lastVisibleElementIndex() + this._buffer;
+        } else {
+            return this.lastVisibleElementIndex() + (this._buffer % this.lastVisibleElementIndex());
+        }
+    }
+
+    public get leadingPlaceholderHeight(): number {
+        return this._leadingPlaceholderHeight;
+    }
+
+    public updateLeadingPlaceholderHeight() {
+        let maxHeight: number = this.totalLines.length * this.lineHeight;
+
+        console.table({
+            'firstRenderedElementIndex: ': this.firstRenderedElementIndex(),
+            'firstVisibleElementIndex: ': this.firstVisibleElementIndex(),
+            'currentScrollPosition': this.currentScrollPosition,
+            'lineHeight: ': this.lineHeight,
+            'totalLinesLength: ': this.totalLines.length,
+            'maxHeight: ': maxHeight
+        })
+
+        if (this.leadingPlaceholderHeight <= maxHeight) {
+            if (this.firstVisibleElementIndex() > this.buffer) {
+                this._leadingPlaceholderHeight = (this.firstVisibleElementIndex() - this.buffer) * this.lineHeight;
+            } else {
+                this._leadingPlaceholderHeight = 0;
+            }
+        }
+    }
+
+    private trailingPlaceholderHeight(): number {
+        return (this.totalLines.length - this.lastVisibleElementIndex() + this._buffer) * this._lineHeight;
+    }
+
+    public getProperties() {
+        return {
+            leadingPlaceholderHeight: this._leadingPlaceholderHeight,
+            firstRenderedElementIndex: this.firstRenderedElementIndex(),
+            lastRenderedElementIndex: this.lastRenderedElementIndex(),
+            trailingPlaceholderHeight: this.trailingPlaceholderHeight()
+        }
     }
 }
